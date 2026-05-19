e92e05d7f6755089cf64f7188a5a0e9940d59d43
Po2432 <202832399+Po2432@users.noreply.github.com>
2026-05-19 10:02:49 +0300

    Add service worker for caching assets

---
+++ docs/sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'lumberboom-v2';
+const ASSETS = [
+    './',
+    './index.html',
+    './play.html',
+    './style.css',
+    './game.js',
+    './manifest.json',
+    './logo.png'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => response || fetch(event.request))
+    );
+});
